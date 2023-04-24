4503e5d95ecedcade53225e2feb85c5fa33a6573
GAUSS Destin <destinmoutekdg.com>
2023-04-24 11:11:26 +0200

    Passage de btech-formation à btech-expert.fr

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,34 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Banner;
-use App\Entity\Footer;
-use App\Entity\News;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->entityManager = $entityManager;
-    }
-
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        $banner = $this->entityManager->getRepository(Banner::class)->findAll();
-        $work = $this->entityManager->getRepository(News::class)->findAll();
-        $footer = $this->entityManager->getRepository(Footer::class)->findAll();
-
         return $this->render('home/index.html.twig', [
-            'images' => $banner,
-            'works' => $work,
-            'footers' => $footer,
+            'controller_name' => 'HomeController',
         ]);
     }
 }
